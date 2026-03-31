01c18a7a046e3af63be7ad07461c3e80565e1ff7
Olimpia Maria Crisan <Crisan.Da.Olimpia@student.utcluj.ro>
2026-03-31 21:27:06 +0300

    Create FileCrawler

---
+++ SDAssig/FileCrawler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using SDAssig;
+
+namespace testproiect
+{
+	public class FileCrawler
+	{
+		private DatabaseManager db;
+
+		public FileCrawler(DatabaseManager database)
+		{
+			db = database;
+		}
+
+		public void IndexDirectory(string targetDirectory)
+		{
+			var files = Directory.GetFiles(targetDirectory, "*.*", SearchOption.AllDirectories)
+				.Where(f => f.EndsWith(".txt") || f.EndsWith(".cs") || f.EndsWith(".md")).ToList();
+
+			foreach (string filePath in files)
+			{
+				try
+				{
+					string content = string.Join(" ", File.ReadLines(filePath).Take(3));
+
+					string lastMod = File.GetLastWriteTime(filePath).ToString();
+					db.SaveFile(filePath, content, lastMod);
+
+				}
+				catch (Exception ex)
+				{
+
+				}
+			}
+		}
+
+	}
+}
